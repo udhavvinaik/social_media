6ba52a80de396b2330bbb626a6c4ff08103f01b7
udhavvinaik <udhavvinaik@gmail.com>
2025-04-22 14:41:21 +0530

    just

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
-/* CONFIGURATIONS */
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-/* FILE STORAGE */
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
@@ -43,7 +43,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-/* ROUTES WITH FILES */
+
 app.post("/auth/register", upload.single("picture"), register);
 
 
@@ -57,13 +57,11 @@ app.post("/posts", verifyToken, upload.fields([
 
 
 
-
-/* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-/* MONGOOSE SETUP */
+
 const PORT = process.env.PORT || 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -73,7 +71,6 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME */
     //User.insertMany(users);
     //Post.insertMany(posts);
   })
